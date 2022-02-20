70892deebcb642ee897f1f389a0c0faba62177f2
joao <esdeciencia@gmail.com>
2022-02-20 17:42:36 -0400

    first fommit

---
+++ ExGymManage.Web/ExGymManage.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExGymManage.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        //GET: /Attendance/MemberIn/
+        public IActionResult MemberIn()
+        {
+            return View();
+        }
+        //GET: /Attendance/MemberOut/
+        public IActionResult MemberOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExGymManage.Web/ExGymManage.Web/wwwroot/js/main.js
@@ -0,0 +1,35 @@
+﻿var func = () => {
+    var entrada = document.getElementById("inputMember");
+    var text = document.getElementById("text-returned");
+    text.innerHTML = "";
+
+    if (entrada.name == "entrada") {
+        text.innerHTML += "Entrada registrada para el miembro  <b>" + entrada.value + "</b>" + " a la hora: <b>" + moment(new Date()).format('hh:mm:ss') + "</b>";
+    } else if (entrada.name == "salida") {
+        text.innerHTML += "Salida registrada para el miembro  <b>" + entrada.value + "</b>" + " a la hora: <b>" + moment(new Date()).format('hh:mm:ss') + "</b>";
+    }
+
+    $("#exampleModal").show();
+
+    setTimeout(function () {
+        $("#exampleModal").hide();
+    }, 3000);
+}
+
+document.querySelector('#register').addEventListener("click", function () {
+    func();
+});
+
+document.querySelector('#btn-close').addEventListener("click", function () {
+    $("#exampleModal").hide();
+});
+
+document.querySelector('#inputMember').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        func();
+        e.preventDefault();
+    }
+});
+
+
+
